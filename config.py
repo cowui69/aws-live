f423d320799e24a76f6fbeae43c705962c06973f
cowui <46859298+cowui69@users.noreply.github.com>
2024-04-16 15:47:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cxw42owog8u8.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "yapengwui-employee"
+customregion = "us-east-1"
+
